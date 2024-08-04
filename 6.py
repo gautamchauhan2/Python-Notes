8e966a13acdca9de90a9504697a81bfe78110f21
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-04 22:02:13 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,11 @@
+#traffic light code
+light=input("light:")
+if(light=="red"):
+    print("Stop")
+elif(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("look")
+else:
+    print("light is broken")
+    
