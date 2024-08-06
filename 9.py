83f081f06995c322b960b60df75d7e08c7bea890
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-06 09:52:41 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,29 @@
+#aritmetic operators
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#remainder
+print(a**b)#a^b
+
+#relational operator
+
+a=50
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assignment operator
+
+a=50
+a+=50
+print(a)
+
+
