4c9d4aae6ce759e1cf71e0a5f5d4574e7f694633
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-07 18:11:03 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,6 @@
+#wap to input 2 int number a and b ,Print True if a greater than or equal to b.If not print false
+
+num1=int(input("Enter a First Number:"))
+num2=int(input("Enter a Second Number:"))
+
+print(num1>=num2)
