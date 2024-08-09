7b8fda665645b650bab500de23b778a21db1dd26
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-09 22:23:36 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,16 @@
+# Python program to find factorial of a given number
+
+#importing the math function
+import math
+
+#Defining the factorial() function to find factorial
+def factorial(num):
+	return(math.factorial(num))
+
+
+# Input function to get the number from user
+num = int(input('Please enter a number to find the factorial: '))
+
+#Printing the factorial of the given number
+print("The factorial of the given number", num, "is",
+	factorial(num))
