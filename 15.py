1ab0ae1a25159ed3752ab5a27ad4ac67c8aef9ae
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-12 17:54:43 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,25 @@
+#list
+marks=[86,85,96,98,78]
+student=["karan",85,"delhi"]
+print(marks[3])   #marks of 3rd index
+print(student[0]) #student of 0 index
+
+#list slicing
+
+marks=[87,64,33,95,76]
+print(marks[1:4])
+print(marks[ :4])
+print(marks[1:])
+print(marks[-3:-1])
+
+#list methods
+
+list=[2,1,3]
+#print(list.append(4))
+#print(list.sort())
+#print(list.sort(reverse=True))
+#print(list.reverse())
+#print(list.insert(1,5))
+#print(list.remove(2))
+print(list.pop(1))
+print(list)
