d1878da5e50abf552c0da641d363ecbf39f83b65
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-13 20:05:27 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,14 @@
+#wap to ask the user to enter names of their 3 favorite movies &store them in a list 
+movies=[]
+mov1=input("Enter the Movies1:")
+mov2=input("Enter the Movies1:")
+mov3=input("Enter the Movies1:")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
+
+
+
