2a8e34e81d0a6960b3c004e00d7d8c56647184a5
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-15 18:32:47 +0530

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,7 @@
+#store following word meanings in a python dictionary table: table"a piece of furniture", "list of facts &figures ". cat: "small animal"
+
+dict={
+    "table":"a piece of furniture ""list of facts &figures ",
+    "cat": "a small animal"
+     }
+print(dict)     
