697e97d726881911066ca3e7b43d34d8122b6816
Gautam Chauhan <2002gautam.chauhan@gmail.com>
2024-08-15 18:32:58 +0530

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,4 @@
+#you are given a list of subjects for students. Assume one classroom is required for 1subject. How many classrooms are needed by all students.
+
+set={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(set)
